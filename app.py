9a84600cec05c282a7aa77025597b7991f68923b
mzalazar01 <Mauro.Zalazar@sybogames.com>
2022-02-15 13:18:57 -0300

    adding logging system and removing temp file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from google.cloud import bigquery
 from flask import Flask, request, jsonify
 import requests as req
+import logging
 import gzip
 import json
 import csv
@@ -10,6 +11,16 @@ import os
 app = Flask(__name__)
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Logging definition
+logging.getLogger("urllib3").propagate = False
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)  # or any other level
+logger.addHandler(ch)
+
 
 @app.route("/", methods=['GET'])
 def hello_word():
@@ -59,24 +70,51 @@ def get_is_data(ds, project, requests_file, token):
 
             config['parameters'].update(_dict)
 
+            logger.info(
+                f"Reading data from appkey {config['parameters']['appKey']} and date {config['parameters']['date']}")
+
+
+            logger.info(
+                f'Getting data from IR Api: {datetime.now().strftime("%H:%M:%S")}')
+
             url = req.get(config['baseURL'], headers=header,
                           params=config['parameters']).json()
+
+            #Remove current csv file
+
+            csv_path = os.path.join(BASE_PATH, 'test.csv.gz')
+            
+            if os.path.exists(csv_path):
+                os.remove(csv_path)
+
             # Hit API and get link to file
+
+            logger.info(
+                f'Getting reports file: {datetime.now().strftime("%H:%M:%S")}')
+
             r = req.get(url['urls'][0], stream=True)
 
-            csv_path = os.join(BASE_PATH, 'test.csv.gz')
-
+            
             # Download the content of the request into a file
             with open(csv_path, 'wb') as f:
                 f.write(r.raw.read())
 
+
+            logger.info(
+                f'Unzipping and parsing data from reports file: {datetime.now().strftime("%H:%M:%S")}')
+            
             with gzip.open(csv_path, "rt", newline="") as file:
                 reader = csv.DictReader(file, delimiter=',', quotechar="'")
+
+                logger.info(
+                f'Uploading reports file to table: {datetime.now().strftime("%H:%M:%S")}')
 
                 rows = list(reader)
                 for batch in batch_rows(rows, 10000):
                     pass
                     #insert_is_data(project_id=project, dataset_id='stg_tb', table_id='', rows=batch)
+                logger.info(
+                f'Finished uploading reports file to table: {datetime.now().strftime("%H:%M:%S")}')
 
 
 def batch_rows(iterable, n=1):
